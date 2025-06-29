1a3860e4d19a9103c67e81976059b23496657de8
mattdeva <matt.neel.10@gmail.com>
2025-06-29 18:04:54 -0400

    method to get count. values flexible for user input.

---
+++ blackjack_simulator/simulation/count.py
@@ -0,0 +1,58 @@
+from blackjack_simulator.components.deck import Deck
+from blackjack_simulator.components.enums import CardValue
+from blackjack_simulator.components.card import card_value_lookup
+
+ValueList = list[str|int|CardValue]
+
+class CountConfig:
+
+    def __init__(
+        self,
+        plus_list:ValueList,
+        neutral_list:ValueList,
+        minus_list:ValueList,
+    ):
+        self.plus_list = self._get_card_values(plus_list)
+        self.neutral_list = self._get_card_values(neutral_list)
+        self.minus_list = self._get_card_values(minus_list)
+
+    @classmethod
+    def from_low_high_cutoffs(cls, low:str|int|CardValue, high:str|int|CardValue):
+        low,high = cls._get_card_values([low, high])
+
+        card_values = list(CardValue)
+        
+        low_indx, high_indx = card_values.index(low)+1, card_values.index(high)
+
+        plus_list = card_values[:low_indx]
+        neutral_list = card_values[low_indx:high_indx]
+        minus_list = card_values[high_indx:]
+
+        return cls(plus_list, neutral_list, minus_list)
+        
+    @staticmethod
+    def _get_card_values(input_:ValueList):
+        out_list = []
+        for i in input_:
+            if isinstance(i, CardValue):
+                out_list.append(i)
+            else:
+                out_list.append(card_value_lookup[i])
+        return out_list
+
+def get_count(deck:Deck, count_config:CountConfig):
+    count = 0
+    for card in deck.drawn_cards:
+        if card.value in count_config.minus_list:
+            count -= 1
+        if card.value in count_config.plus_list:
+            count += 1
+    return count
+
+# cc = CountConfig.from_low_high_cutoffs(6, 10)
+
+# (
+#     cc.minus_list,
+#     cc.neutral_list,
+#     cc.plus_list,
+# )
