f70f83e25c4d7cb23a6bd843f1f1f2a8e390aa0c
mattdeva <matt.neel.10@gmail.com>
2025-07-07 23:16:23 -0400

    add metrics

--- blackjack_simulator/simulation/simulator.py
+++ blackjack_simulator/simulation/simulator.py
@@ -1,50 +1,71 @@
-import random
 import pandas as pd
 from itertools import product
+from dataclasses import dataclass
 
 from blackjack_simulator.components.chart import Chart
 from blackjack_simulator.components.bet import Bet
-from blackjack_simulator.simulation.functions import deal, shuffle_cut_deck
+from blackjack_simulator.components.card import Card
+from blackjack_simulator.components.enums import Action
+from blackjack_simulator.simulation.counter import Counter
+from blackjack_simulator.simulation.functions import deal, shuffle_cut_deck, get_count
 from blackjack_simulator.simulation.game import run_dealer_actions, run_player_actions, calculate_payout
 
 from typing import Sequence
 
 
-def summarize(sequence:Sequence[tuple[float]]) -> dict[str, float]:
-    #     tuples = [
-    #     (1,0),
-    #     (1,2),
-    #     (1,1.5),
-    #     (2,0),
-    #     (2,4),
-    #     (1,0)
-    # ] -> {'total_buyin': 8, 'total_payout': 7.5, 'total_obs': 6}
-    total_buyin = sum([t[0] for t in sequence])
-    total_payout = sum([t[1] for t in sequence])
-    return {
-        'total_buyin' : total_buyin,
-        'total_payout' : total_payout,
-        'total_obs' : len(sequence),
-        'roi':round(total_payout/total_buyin*100,1) if len(sequence) > 0 else 'N/A'
-    }
+@dataclass
+class Record:
+    ''' Way to record and hands, situations, and results '''
+    hand_id: str
+    player_hand_lookup: str|int
+    dealer_upcard_value: str|int
+    count: int
+    chart: int
+    bet_units: float
+    action: Action
+    hit_card: Card
+    final_payout: float 
 
-def result_dict_to_df(results_dict:dict, metric='roi'):
-    metric_dict = {k:v[metric] for k,v in results_dict.items()}
-    df = pd.DataFrame.from_dict(metric_dict, orient='index').reset_index()
+    @property
+    def result(self) -> int:
+        return 1 if self.final_payout > self.bet_units else 0 if self.final_payout == self.bet_units else -1
+    
+    @property
+    def chart_lookup(self) -> tuple[str]:
+        return (self.player_hand_lookup, self.dealer_upcard_value)
+    
+    def to_dict(self) -> dict:
+        d = self.__dict__
+        d.update({'result':self.result})
+        return d
 
-    # split index column into rows, columns
-    df[['player_hand', 'columns']] = pd.DataFrame(df['index'].tolist(), index=df.index)
+def get_hand_id(x: int, digits: int) -> str:
+    if x < 0:
+        raise ValueError("Input must be a positive integer.")
+    return str(x).zfill(digits)
 
-    # remove index column
-    df = df.drop('index', axis=1)
+def summarize_str_to_dict(str_:str) -> dict[str,float]:
+    return {k: float(v) for k, v in (item.split(":") for item in str_.split(", "))}
 
-    # pivot
-    df = df.pivot(index='player_hand', columns='columns', values=0).reset_index()
+def summarize_wins(records:list[Record]) -> str:
+    ''' return win - loss - push as percentages of records '''
+    wins = len([record for record in records if record.result == 1])
+    pushes = len([record for record in records if record.result == 0])
+    losses = len([record for record in records if record.result == -1])
+    return f"Wins:{wins}, Pushes:{pushes}, Losses:{losses}" # NOTE: may want to format ast or json. ok for now.
 
-    # stupid
-    df.columns.name = ''
+def summarize_profit(records:list[Record]) -> str:
+    buyin = sum([record.bet_units for record in records]) 
+    payout = sum([record.final_payout for record in records])
+    return f"Buyin:{buyin}, Payout:{payout}, Net:{round(payout-buyin,1)}" # NOTE: may want to format ast or json. ok for now.
 
-    return df
+def summarize_ev(records:list[Record]) -> str:
+    # NOTE: not 100% sure about this calculation...
+    wins_dict = summarize_str_to_dict(summarize_wins(records))
+    profit_dict = summarize_str_to_dict(summarize_profit(records))
+    observations = sum(list(wins_dict.values()))
+    ev = round(profit_dict['Net'] / observations, 4) if observations > 0 else 0
+    return f"Net_Units:{profit_dict['Net']}, Obs:{observations}, EV:{ev}"
 
 class Simulator:
     def __init__(
@@ -54,7 +75,8 @@ class Simulator:
             n_decks: int = 6,
             max_splits:int = 4,
             blackjack_payout:float = 3/2,
-            surrender_payout:float = 1/2
+            surrender_payout:float = 1/2,
+            counter:Counter|None = None
         ):
         self.chart = chart
         self.dealer_hit_soft_17 = dealer_hit_soft_17
@@ -62,29 +84,16 @@ class Simulator:
         self.max_splits = max_splits
         self.blackjack_payout = blackjack_payout
         self.surrender_payout = surrender_payout
-        
-        # TEMP -- fix to update several strategies based on deck hotness
-        self._results_dict = None
+        self.counter = Counter.from_low_high_cutoffs(6,10) if counter is None else counter
+        self._records:list[Record] = []
 
-    @staticmethod
-    def _track_results(results_dict:dict, bet:Bet, payout:float) -> None:
-        # NOTE: in future may want to track how win/loss happened (bust/showdown) (prob would have to change game a lil)
-        for action in bet.history:
-            results_dict[(action[1], action[2])].append((bet.units, payout))
-
-    @staticmethod
-    def _get_empty_results(chart:Chart):
-        results_dict = {}
-        hand_combos =  list(product(chart.df.index.to_list(), chart.df.columns.to_list()))
-        for hand_combo in hand_combos:
-            results_dict[hand_combo] = []
-        return results_dict
+    @property
+    def lookup_keys(self):
+        # NOTE: may have to change if/when incorporate flexible charts
+        return list(product(self.chart.df.index.to_list(), self.chart.df.columns.to_list()))
     
-    def result_dict_to_df(self, metric='roi') -> pd.DataFrame:
-
-        # NOTE: think this will error if try to run without running simulation... should fix that.
-
-        metric_dict = {k:v[metric] for k,v in self.results_dict.items()}
+    @staticmethod
+    def _metric_dict_to_df(metric_dict:dict) -> pd.DataFrame:
         df = pd.DataFrame.from_dict(metric_dict, orient='index').reset_index()
 
         # split index column into rows, columns
@@ -101,26 +110,68 @@ class Simulator:
 
         return df
     
-    @property
-    def results_dict(self) -> dict:
-        if self._results_dict is None:
-            self._results_dict = self._get_empty_results(self.chart) # NOTE: would need to be updated if using multiple charts
-        return self._results_dict
+    def _filter_records_by_lookup(self, lookup:tuple[int,str]) -> list[Record]:
+        return [r for r in self._records if r.chart_lookup == lookup]
+    
+    def records_to_df(self):
+        if len(self._records) == 0:
+            print('No records found. Run simulation.')
+        else:
+            return pd.DataFrame([r.to_dict() for r in self._records])
+    
+    def output_chart(self, typ:str) -> pd.DataFrame:
 
-
-    def run(self, hands = 10, overwrite:bool=False) -> None:
-
-        if self._results_dict is not None and not overwrite:
-            raise ValueError(f'non-emtpy results. must set overwrite to True') # safeguard agaisnt overwriting (temp prob)
-                
-        deck, stop= shuffle_cut_deck(self.n_decks)
-        hand_counter = 0
-        shoe_counter = 0
+        if typ not in ['profit', 'win', 'ev']: # NOTE: should make enums for this but too lazy rn...
+            raise ValueError(f"output_chart typ must be in ['profit', 'win', 'ev']. got {typ}")
         
+        metric_dict = {}
+        for lookup in self.lookup_keys:
+            records = self._filter_records_by_lookup(lookup)
+            if typ == 'profit':
+                output_str = summarize_profit(records)
+            if typ == 'win':
+                output_str = summarize_wins(records)
+            if typ == 'ev':
+                output_str = summarize_ev(records)
+
+            metric_dict[lookup] = output_str
+            
+        return self._metric_dict_to_df(metric_dict)
+        
+    def output_summary(self):
+        return (
+            summarize_wins(self._records),
+            summarize_profit(self._records),
+            summarize_ev(self._records)
+        )
+    
+    def run(self, hands = 10, overwrite:bool=False) -> None:
+        
+        # simulation / overwrite stuff
+        if len(self._records) > 0 and not overwrite:
+            raise ValueError(f'non-emtpy records. must set overwrite to True') # safeguard agaisnt overwriting (temp prob)
+        
+        self._records = []
+                
+        # shoe stuff
+        deck, stop= shuffle_cut_deck(self.n_decks) # stop indicates the position of the cut card
+        shoe_counter = 0
+
+        # create ids of minimum length 6th
+        id_digits = len(str(hands)) if len(str(hands)) > 6 else 6
+        
+        hand_counter = 0
         while hand_counter < hands:
+            hand_id = get_hand_id(hand_counter, id_digits) 
+
+            # NOTE: doing pre-hand-count for now, but should update for real-time count after
+            pre_hand_count = get_count(deck.drawn_cards, self.counter)
+
+            # shoe stuff
             if shoe_counter > stop:
                 deck,stop = shuffle_cut_deck(self.n_decks)
             start_n_cards = len(deck) # lazy way to get how many cards used per round...
+            
             player_hand, dealer_hand = deal(deck)
 
             bet = Bet(player_hand, units=1) # TODO: flexible units involving pressing & deck cound
@@ -129,10 +180,21 @@ class Simulator:
 
             for bet in bets:
                 payout = calculate_payout(bet, dealer_hand, self.blackjack_payout, self.surrender_payout)
-                self._track_results(self.results_dict, bet, payout)
+
+                for history in bet.history:
+                    self._records.append(Record(
+                        hand_id, 
+                        history.player_lookup_value, 
+                        history.dealer_upcard_value,
+                        pre_hand_count,
+                        1, # NOTE: only one chart type allowed for now
+                        bet.units, 
+                        history.action, 
+                        history.hit_card,
+                        payout
+                    ))
 
             end_n_cards = len(deck)
             shoe_counter += start_n_cards - end_n_cards
 
             hand_counter +=1
-        self._results_dict = {k:summarize(v) for k,v in self._results_dict.items()}
