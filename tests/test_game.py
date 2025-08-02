341ef9c1d71defadb40436f88a6078f255177b78
mattdeva <matt.neel.10@gmail.com>
2025-08-01 20:41:11 -0400

    bug fixes

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,9 +11,9 @@ from blackjack_simulator.simulation.game import calculate_payout
     (Bet(PlayerHand([Card('A','S'), Card('J','S')]), state=HandState.BLACKJACK), DealerHand([Card('A','H'), Card(10,'D')]), 1), # blackjack push
     (Bet(PlayerHand([Card(6,'S'), Card('J','S')]), state=HandState.SURRENDER), DealerHand([Card('A','H'), Card(9,'D')]), .5), # surrender
     (Bet(PlayerHand([Card(7,'S'), Card('J','S')])), DealerHand([Card('K','H'), Card(6,'D'), Card(7,'S')]), 2), # dealter bust
-    (Bet(PlayerHand([Card(7,'S'), Card('J','S')]), state=HandState.DOUBLE), DealerHand([Card('K','H'), Card(6,'D'), Card(7,'S')]), 4), # dealter bust + double
+    (Bet(PlayerHand([Card(7,'S'), Card('J','S')]), state=HandState.DOUBLE), DealerHand([Card('K','H'), Card(6,'D'), Card(7,'S')]), 2), # dealter bust + double
     (Bet(PlayerHand([Card(9,'S'), Card('J','S')])), DealerHand([Card('A','H'), Card(7,'D')]), 2), # player total >
-    (Bet(PlayerHand([Card(9,'S'), Card('J','S')]), state=HandState.DOUBLE), DealerHand([Card('A','H'), Card(7,'D')]), 4), # player total > + double
+    (Bet(PlayerHand([Card(9,'S'), Card('J','S')]), state=HandState.DOUBLE), DealerHand([Card('A','H'), Card(7,'D')]), 2), # player total > + double
     (Bet(PlayerHand([Card(9,'S'), Card('J','S')])), DealerHand([Card('A','H'), Card(9,'D')]), 0), # dealer total >
     (Bet(PlayerHand([Card(8,'S'), Card('J','S')])), DealerHand([Card('A','H'), Card(7,'D')]), 1), # push
 ])
